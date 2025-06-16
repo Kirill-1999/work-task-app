7d2c313fe00391e9dac5ec3a2de6c4c2fabacac3
krutakov <krutakovegor43@gmail.com>
2025-06-17 00:42:57 +0300

    Integrate useLogin hook into SignInCard for handling login mutations, replacing console log with API call for form submission.

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -23,8 +23,11 @@ import {
     FormMessage,
 } from "@/components/ui/form";
 import { loginSchema } from "../schemas";
+import { useLogin } from "../api/use-login";
 
 export const SignInCard = () => {
+    const { mutate } = useLogin();
+
     const form = useForm<z.infer<typeof loginSchema>>({
         resolver: zodResolver(loginSchema),
         defaultValues: {
@@ -34,8 +37,8 @@ export const SignInCard = () => {
     });
 
     const onsubmit = (values: z.infer<typeof loginSchema>) => {
-        console.log({ values });
-    }
+        mutate(values);
+    };
 
     return (
         <Card className="w-full h-full md:w-[487px] border-none shadow-none">
