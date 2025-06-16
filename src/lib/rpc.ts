47bb0abedcd5baaf1a955fc033311ee53f289556
krutakov <krutakovegor43@gmail.com>
2025-06-17 00:30:17 +0300

    Update .gitignore to include .env*.local files and refactor RPC client to use environment variable for server URL.

--- src/lib/rpc.ts
+++ src/lib/rpc.ts
@@ -1,6 +1,5 @@
 import { hc } from "hono/client";
 
 import { AppType } from "@/app/api/[[...route]]/route";
-import App from "next/app";
 
-export const client = hc<AppType>("http://localhost:3000");
+export const client = hc<AppType>(process.env.NEXT_PUBLIC_APP_URL!);
