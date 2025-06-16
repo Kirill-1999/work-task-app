9d5fd4890f8d4ba202659b87200c7f57cb130799
krutakov <krutakovegor43@gmail.com>
2025-06-16 16:49:47 +0300

    Add react-icons dependency and enhance SignInCard component with additional input fields and buttons for login options

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,4 +1,7 @@
+
+
 import { DottedSeparator } from "@/components/dotted-separator";
+import { Button } from "@/components/ui/button";
 import {
     Card,
     CardContent,
@@ -24,11 +27,43 @@ export const SignInCard = () => {
                         required
                         type="email"
                         value={""}
-                        onChange={() => {}}
+                        onChange={() => { }}
                         placeholder="Enter email address"
                         disabled={false}
-                        />
-                    </form>
+                    />
+                    <Input
+                        required
+                        type="password"
+                        value={""}
+                        onChange={() => { }}
+                        placeholder="Enter password"
+                        disabled={false}
+                        min={8}
+                        max={255}
+                    />
+                    <Button disabled={false} size="lg" className="w-full">
+                        Login 
+                    </Button>
+                </form>
+            </CardContent>
+            <div className="px-7">
+                <DottedSeparator />
+            </div>
+            <CardContent className="p-7 flex flex-col gap-y-4">
+            <Button 
+            disabled={false}
+            variant="secondary"
+            size="lg"
+            className="w-full">
+            Login with Google
+            </Button>
+            <Button 
+            disabled={false}
+            variant="secondary"
+            size="lg"
+            className="w-full">
+            Login with @Mail.ru
+            </Button>
             </CardContent>
         </Card>
     );
