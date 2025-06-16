678511bde54778b78ad02b828f167829563a7f77
holod <holodovkirill1999@gmail.com>
2025-06-16 21:31:19 +0500

    Refactor SignInCard component to integrate Zod validation and react-hook-form for form handling. Update input fields to use Form components for better structure and validation.

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,5 +1,9 @@
+import { z } from "zod";
+
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import { DottedSeparator } from "@/components/dotted-separator";
 import { Button } from "@/components/ui/button";
@@ -10,8 +14,28 @@ import {
     CardTitle
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage,
+} from "@/components/ui/form";
+
+const formSchema = z.object({
+    email: z.string().email(),
+    password: z.string(),
+});
 
 export const SignInCard = () => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    });
+
     return (
         <Card className="w-full h-full md:w-[487px] border-none shadow-none">
             <CardHeader className="flex items-center justify-center text-center p-7">
@@ -23,29 +47,36 @@ export const SignInCard = () => {
                 <DottedSeparator />
             </div>
             <CardContent className="p-7">
-                <form className="space-y-4">
-                    <Input
-                        required
-                        type="email"
-                        value={""}
-                        onChange={() => { }}
-                        placeholder="Enter email address"
-                        disabled={false}
-                    />
-                    <Input
-                        required
-                        type="password"
-                        value={""}
-                        onChange={() => { }}
-                        placeholder="Enter password"
-                        disabled={false}
-                        min={8}
-                        max={255}
-                    />
-                    <Button disabled={false} size="lg" className="w-full">
-                        Login
-                    </Button>
-                </form>
+                <Form {...form}>
+                    <form className="space-y-4">
+                        <FormField
+                        name="name"
+                            render={({ field }) => (
+                                <Input
+                                    required
+                                    type="email"
+                                    value={""}
+                                    onChange={() => { }}
+                                    placeholder="Enter email address"
+                                    disabled={false}
+                                />
+                            )}
+                        />
+                        <Input
+                            required
+                            type="password"
+                            value={""}
+                            onChange={() => { }}
+                            placeholder="Enter password"
+                            disabled={false}
+                            min={8}
+                            max={255}
+                        />
+                        <Button disabled={false} size="lg" className="w-full">
+                            Login
+                        </Button>
+                    </form>
+                </Form>
             </CardContent>
             <div className="px-7">
                 <DottedSeparator />
