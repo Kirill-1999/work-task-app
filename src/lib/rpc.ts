25860c60e842d5f9430b36a16175cbb45c1c5a7d
krutakov <krutakovegor43@gmail.com>
2025-06-17 00:27:02 +0300

    Enhance RPC client setup by importing Next.js App component and establishing a client connection to the local server.

--- src/lib/rpc.ts
+++ src/lib/rpc.ts
@@ -1,3 +1,6 @@
 import { hc } from "hono/client";
 
 import { AppType } from "@/app/api/[[...route]]/route";
+import App from "next/app";
+
+export const client = hc<AppType>("http://localhost:3000");
